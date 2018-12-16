three poiters fit a circle algorithm fixed when Int subtraction OVERFLOW.

RasancFitCircle.py
@@ -46,6 +46,8 @@ def fitNum(pointers, circle, threshold, inliers, consensus_pointers=None):
     num = 0
     data_size = len(pointers)
     inliers_size = len(inliers)
+    center = (circle[0], circle[1])
+    radius = circle[2]
     for i in range(0, data_size):
         is_inliers = False
         for j in range(0, inliers_size):
@@ -53,7 +55,9 @@ def fitNum(pointers, circle, threshold, inliers, consensus_pointers=None):
                 is_inliers = True
         if is_inliers:
             continue
-        if math.fabs(distance(pointers[i], circle[2])) < threshold:
+        dis = distance(pointers[i], center) - radius
+        print(dis)
+        if math.fabs(distance(pointers[i], center) - radius) < threshold:
             num += 1
             consensus_pointers.append(pointers[i])
     return num
@@ -71,20 +75,28 @@ def getCircle(pointer1, pointer2, pointer3):
     :param pointer3:
     :return:
     """
-    x1 = pointer1[0]
-    x2 = pointer2[0]
-    x3 = pointer3[0]
-
-    y1 = pointer1[1]
-    y2 = pointer1[1]
-    y3 = pointer1[1]
     circle = np.zeros(shape=(3, 1), dtype=np.float16)
-    circle[0] = (x1 * x1 + y1 * y1) * (y2 - y3) + (x2 * x2 + y2 * y2) * (y3 - y1) + (x3 * x3 + y3 * y3) * (y1 - y2)
-    if (2 * (x1 * (y2 - y3) - y1 * (x2 - x3) + x2 * y3 - x3 * y2)) != 0:
-        circle[0] /= (2 * (x1 * (y2 - y3) - y1 * (x2 - x3) + x2 * y3 - x3 * y2));
-    if (x1 * x1 + y1 * y1) * (x3 - x2) + (x2 * x2 + y2 * y2) * (x1 - x3) + (x3 * x3 + y3 * y3) * (x2 - x1) != 0:
-        circle[1] = (x1 * x1 + y1 * y1) * (x3 - x2) + (x2 * x2 + y2 * y2) * (x1 - x3) + (x3 * x3 + y3 * y3) * (x2 - x1)
-    circle[1] /= (2 * (x1 * (y2 - y3) - y1 * (x2 - x3) + x2 * y3 - x3 * y2))
+    x1 = float(pointer1[0])
+    x2 = float(pointer2[0])
+    x3 = float(pointer3[0])
+
+    y1 = float(pointer1[1])
+    y2 = float(pointer2[1])
+    y3 = float(pointer3[1])
+
+    a = x1 - x2
+    b = y1 - y2
+    c = x1 - x3
+    d = y1 - y3
+    e = ((x1 * x1 - x2 * x2) + (y1 * y1 - y2 * y2)) / 2.0
+    f = ((x1 * x1 - x3 * x3) + (y1 * y1 - y3 * y3)) / 2.0
+    det = b * c - a * d
+
+    if math.fabs(det) < 1e-5:
+        circle = [0, 0, -1]
+        return circle
+    circle[0] = -(d * e - b * f) / det
+    circle[1] = -(a * f - c * e) / det
     circle[2] = math.sqrt((circle[0] - x1) * (circle[0] - x1) + (circle[1] - y1) * (circle[1] - y1))
     return circle
 

test_randomSampleConsensus.py
@@ -2,6 +2,8 @@ from unittest import TestCase
 import unittest
 import numpy as np
 import RasancFitCircle as rasanc
+import cv2
+import PlotUtil as plot
 
 
 class TestRandomSampleConsensus(TestCase):
@@ -25,11 +27,10 @@ class TestRandomSampleConsensus(TestCase):
             b = np.random.randint(low=0, high=255)
             g = np.random.randint(low=0, high=255)
             r = np.random.randint(low=0, high=255)
-            print((b, g, r))
             circle_img[p[0]][p[1]] = (b, g, r)
         # cv2.circle(circle_img, center=(R, R), radius=R, color=(0, 255, 0), thickness=1)
-        # plot.subImage(src=circle_img, index=1, title='Circle')
-        # plot.show()
+        plot.subImage(src=circle_img, index=1, title='Circle')
+        plot.show()
         best_circle, max_fit_num, best_consensus_pointers = rasanc.randomSampleConsensus(data=pointers,
                                                                                          max_iterations=40,
                                                                                          dst_threshold=50,
@@ -43,7 +44,6 @@ class TestRandomSampleConsensus(TestCase):
 
 def suite():
     suite = unittest.TestSuite()
-
     suite.addTest(TestRandomSampleConsensus('test_randomSampleConsensus'))
     return suite
 