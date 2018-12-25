0c158f716aac5d618bbd839d82e4b5ba808da4ea
hjptriplebee <huangjipengnju@gmail.com>
2018-12-25 09:34:14 +0800

    test service sample

---
+++ TestServiceSample.py
@@ -0,0 +1,58 @@
+from Common import *
+from Interface import *
+import requests
+import base64
+import json
+from flask import Flask
+
+# test reader interface
+image = open("image/bileiqi1_1.jpg", "rb")
+imageByte = base64.b64encode(image.read())
+data = json.dumps({
+    "image": imageByte.decode("ascii"),
+    "imageID": "bileiqi1"
+})
+
+r = requests.post("http://127.0.0.1:5000/", data=data.encode("utf-8"))
+
+print(r.text)
+
+
+# test store interface
+# image = open("template/1_1.jpg", "rb")
+# imageByte = base64.b64encode(image.read())
+# data = json.dumps({
+#     "template": imageByte.decode("ascii"),
+#     "imageID": "1",
+#     "config": {
+#       "distance": 10.0,
+#       "horizontal": 10.0,
+#       "vertical": 20.0,
+#       "name": "1_1",
+#       "type": "SF6",
+#       "ROI": {
+#           "x": 200,
+#           "y": 200,
+#           "w": 1520,
+#           "h": 680
+#       },
+#       "startPoint": {
+#           "x": -1,
+#           "y": -1
+#       },
+#       "endPoint": {
+#           "x": -1,
+#           "y": -1
+#       },
+#       "centerPoint": {
+#           "x": -1,
+#           "y": -1
+#       },
+#       "startValue": 0.0,
+#       "totalValue": 2.0
+#     }
+# })
+#
+# r = requests.post("http://127.0.0.1:5000/store", data=data.encode("utf-8"))
+#
+# print(r.text)
