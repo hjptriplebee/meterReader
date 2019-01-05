a123852250fc0f6c46a64626a48cc6825082cf58
szkszk95 <739924940@qq.com>
2019-01-05 21:19:23 +0800

    修改了showLabel检验标注效果，修改了一些错误的标注

--- colorPressure.py
+++ colorPressure.py
@@ -1,4 +1,2 @@
 def colorPressure(image, info):
-    import showLabel as sl
-    sl.showLabel(image, info)
     return 0
--- digitArrester.py
+++ digitArrester.py
@@ -2,7 +2,7 @@
 import math
 import numpy as np
 import cv2 as cv
-from keras.models import load_model
+# from keras.models import load_model
 from Common import *
 
 
